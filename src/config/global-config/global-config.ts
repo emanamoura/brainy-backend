c2ece77f3b5033ac55523177d3318957cf6d938c
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-10-29 21:01:57 -0300

    config: adding global config

---
+++ src/config/global-config/global-config.ts
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+import { PROJECT_ROOT_DIRECTORY } from './constants';
+import { Mode } from './types';
+
+export class GlobalConfig {
+  private _mode: Mode;
+  private _port: number;
+  private _allowedCORSOrigins: string[];
+  private _jwtSecret: string;
+  private _docsUrl: string;
+  constructor() {
+    this._mode = process.env.NODE_ENV ?? 'development';
+    this.loadEnvironmentVariables(this.mode());
+    this._port = Number(process.env.PORT);
+    this._allowedCORSOrigins = process.env.ALLOWED_CORS_ORIGINS?.split(',') ?? ['*'];
+    this._jwtSecret = process.env.JWT_SECRET;
+    this._docsUrl = '/api-docs';
+  }
+
+  mode(): Mode {
+    return this._mode;
+  }
+
+  port(): number {
+    return this._port;
+  }
+
+  allowedCORSOrigins(): string[] {
+    return this._allowedCORSOrigins;
+  }
+
+  docsUrl(): string {
+    return this._docsUrl;
+  }
+
+  private loadEnvironmentVariables(mode: Mode) {
+    const environmentFiles = [
+      path.join(PROJECT_ROOT_DIRECTORY, `.env.${mode}`),
+      path.join(PROJECT_ROOT_DIRECTORY, `.env.${mode}.local`),
+    ];
+
+    environmentFiles.forEach((file) => dotenv.config({ path: file }));
+  }
+}
+
+const globalConfig = new GlobalConfig();
+
+export default globalConfig;
